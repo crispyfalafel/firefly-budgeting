52f9cbb759595c405842eaf81d663753d843f923
Diana Voloshin <diavoloshin@gmail.com>
2021-01-09 15:31:26 -0800

    Pain

--- manage_goals.js
+++ manage_goals.js
@@ -3,11 +3,11 @@ myFunction = function() {
     alert('hai claire')
   }
 
-  var slider = document.getElementById("myRange");
-  var output = document.getElementById("demo");
-  output.innerHTML = slider.value; // Display the default slider value
-  
-  // Update the current slider value (each time you drag the slider handle)
-  slider.oninput = function() {
-    output.innerHTML = this.value;
-  }
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.textContent = slider.value;
+
+slider.onchange = function() {
+  console.log(slider.value)
+  output.textContent = this.value;
+}
