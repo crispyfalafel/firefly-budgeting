b29fefe38aa7c83f7d1a07e6a537647029766853
Jolin Lin <jolinlin5622@hotmail.com>
2021-01-08 20:05:01 -0800

    add landing

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const path = require('path');
 const session = require('express-session');
-
+const mongoose = require('mongoose');
 const indexRouter = require('./Routes/index');
 
 // Initialize app
 const app = express();
 const port = 3000;
+
+// Connect to database
+mongoose.connect('mongodb://localhost:27017/financeDatabase')
+let db = mongoose.connection;
+db.once('open', () => {
+    console.log('database connected');
+})
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
